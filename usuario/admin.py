841036003a7d602f1dca229dab5e846bce7ac6a5
6987k <maurojimsilva@gmail.com>
2023-07-14 15:25:47 -0300

    modified:   usuario/admin.py

--- usuario/admin.py
+++ usuario/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from usuario.models import InfoUsuario
+from usuario.models import InfoUsuario, Resenia, FotoResenia
 
 admin.site.register(InfoUsuario) 
+admin.site.register(Resenia)
+admin.site.register(FotoResenia)
