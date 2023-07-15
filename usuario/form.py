f8961a96bdaa04407e08c3c78c947edba660eeb3
6987k <maurojimsilva@gmail.com>
2023-07-15 14:51:05 -0300

    modified:   db.sqlite3 	modified:   usuario/form.py 	modified:   usuario/templates/usuario/editar_resenia.html 	modified:   usuario/templates/usuario/mostrar_resenia.html 	modified:   usuario/urls.py 	modified:   usuario/views.py

--- usuario/form.py
+++ usuario/form.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.contrib.auth.models import User
+from usuario.models import Resenia, FotoResenia
+
 
 
 class FormularioRegistro(UserCreationForm):
@@ -29,13 +31,27 @@ class FormularioEditarPerfil(UserChangeForm):
 class BuscarResenia(forms.Form):
     titulo = forms.CharField(max_length=20, required=False)
     
-class FormularioEditarResenia(UserChangeForm):
-    titulo = forms.CharField(label='Titulo', max_length=20)
-    subtitulo = forms.CharField(label='Subtitulo', max_length=20)
-    contenido = forms.CharField(label='Contenido', max_length=3000)
-    autor = forms.CharField(label='autor', max_length=20)
-    fecha_de_creacion = forms.DateField(required=False, widget=forms.DateInput(format='%d/%m/%Y'), input_formats=('%d/%m/%Y', ))
-    avatar = forms.ImageField(required=False)
+    
+class FormularioEditarResenia(forms.ModelForm):
+    class Meta:
+        model = Resenia
+        fields = ['titulo', 'subtitulo', 'contenido', 'autor', 'fecha_de_creacion']
+        widgets = {
+            'fecha_de_creacion': forms.DateInput(format='%d/%m/%Y'),
+        }
+
+class FormularioEditarFotoResenia(forms.ModelForm):
+    class Meta:
+        model = FotoResenia
+        fields = ['foto_resenia']
+    
+# class FormularioEditarResenia(UserChangeForm):
+#     titulo = forms.CharField(label='Titulo', max_length=20)
+#     subtitulo = forms.CharField(label='Subtitulo', max_length=20)
+#     contenido = forms.CharField(label='Contenido', max_length=3000)
+#     autor = forms.CharField(label='autor', max_length=20)
+#     fecha_de_creacion = forms.DateField(required=False, widget=forms.DateInput(format='%d/%m/%Y'), input_formats=('%d/%m/%Y', ))
+#     avatar = forms.ImageField(required=False)
     
     
     # class Meta:
--- usuario/urls.py
+++ usuario/urls.py
@@ -3,6 +3,22 @@ from usuario import views
 from django.contrib.auth.views import LogoutView
 app_name = 'usuario'
 
+# urlpatterns = [
+#     path('login/',views.login, name='login'),
+#     path('registro/', views.registro, name='registro'),
+#     path('logout/', LogoutView.as_view(template_name='usuario/logout.html'), name='logout'),
+#     path('perfil', views.perfil, name = 'perfil'),
+#     path('perfil/editar/', views.editar_perfil, name = 'editar_perfil'),
+#     path('perfil/editar/pass', views.CambiarPass.as_view(), name = 'cambiar_pass'),
+#     path('resenias', views.listar_resenias, name = 'resenias'),
+#     path('resenias/postear', views.Postear.as_view(), name = 'postear'),
+#     path('resenias/editar_resenia/<int:pk>/', views.editar_resenia, name = 'editar_resenia'),
+#     path('resenias/eliminar_resenia/<int:pk>/', views.EliminarResenia.as_view(), name = 'eliminar_resenia'),
+#     path('resenias/mostrar/<int:pk>/', views.MostrarResenia.as_view(), name = 'mostrar_resenia'),
+
+
+    
+# ]
 urlpatterns = [
     path('login/',views.login, name='login'),
     path('registro/', views.registro, name='registro'),
@@ -12,11 +28,7 @@ urlpatterns = [
     path('perfil/editar/pass', views.CambiarPass.as_view(), name = 'cambiar_pass'),
     path('resenias', views.listar_resenias, name = 'resenias'),
     path('resenias/postear', views.Postear.as_view(), name = 'postear'),
-    path('resenias/editar_resenia/<int:pk>/', views.editar_resenia, name = 'editar_resenia'),
+    path('resenias/editar_resenia/<int:pk>/', views.editar_resenia, name='editar_resenia'),
     path('resenias/eliminar_resenia/<int:pk>/', views.EliminarResenia.as_view(), name = 'eliminar_resenia'),
-    path('resenias/mostrar/<int:pk>/', views.MostrarResenia.as_view(), name = 'mostrar_resenia'),
-
-
-    
+    path('resenias/mostrar/<int:pk>/', views.MostrarResenia.as_view(), name = 'mostrar_resenia'),   
 ]
-  
