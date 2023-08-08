93e616347a482391d32b5d20856bafdc5bda1cba
6987k <maurojimsilva@gmail.com>
2023-08-08 14:35:50 -0300

    modified:   db.sqlite3 	new file:   usuario/migrations/0016_infousuario_cumpleanios.py 	modified:   usuario/models.py 	modified:   usuario/templates/usuario/perfil.html

--- usuario/models.py
+++ usuario/models.py
@@ -2,9 +2,11 @@ from django.db import models
 from django.contrib.auth.models import User
 from ckeditor.fields import RichTextField
 
+
 class InfoUsuario(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     avatar = models.ImageField(upload_to='avatares', null=True, blank=True)
+    cumpleanios = models.DateField(blank=True, null=True)
     
 class Resenia(models.Model):
     titulo = models.CharField(max_length=20)
